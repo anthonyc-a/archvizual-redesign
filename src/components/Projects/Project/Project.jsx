1ee946ce9eb494bea859e1ec22a641a9b1cbac20
anthonyc-a <anthonyc.animba@gmail.com>
2021-12-31 22:37:00 +0000

    Added guideline image to projects

--- src/components/Projects/Project/Project.jsx
+++ src/components/Projects/Project/Project.jsx
@@ -1,9 +1,15 @@
 import React from "react"
+import img1 from "../../../images/proj1.jpg"
 
 const Project = () => {
   return (
     <a href="/" className="project">
       <div className="project-tag">02—022</div>
+      <div className="project-img">
+        <div className="img-contain">
+          <img src={img1} alt="" />
+        </div>
+      </div>
       <div className="project-info">
         <div className="project-name">Project | 38</div>
         <div className="project-type">Residential</div>
